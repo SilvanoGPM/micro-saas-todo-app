bfebe3453258e92b665e23dbbbc1cd844174e53c
SilvanoGPM <silvanosilvino@hotmail.com>
2024-10-13 00:51:56 -0300

    chore: lint removido do build

--- next.config.js
+++ next.config.js
@@ -11,6 +11,9 @@ const withPWA = require('next-pwa')({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = withBundleAnalyzer(
