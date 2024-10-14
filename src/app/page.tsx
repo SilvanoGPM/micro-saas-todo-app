9f8705a87bcd02e1601e857a5f7950236d0e674c
SilvanoGPM <silvanosilvino@hotmail.com>
2024-10-14 20:37:55 -0300

    feat: página inicial alterada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,17 @@
+import Link from 'next/link';
+
+import { ROUTES } from '$libs/auth/routes';
+import { Button } from '$components/ui/button';
+import { Logo } from '$components/logo';
+
 export default function HomePage() {
-  return <main className="h-dvh w-full">Home</main>;
+  return (
+    <main className="h-dvh w-full flex flex-col gap-4 items-center justify-center">
+      <Logo />
+
+      <Button>
+        <Link href={ROUTES.private.home.path}>Ir para Dashboard</Link>
+      </Button>
+    </main>
+  );
 }
